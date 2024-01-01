2abb5939f3fc77694f14d2ad460e9b6b5182c29c
BilalHammi <Bilalhammi45@gmail.com>
2024-01-01 15:49:13 +0100

    Removed API file, added into main.js + Fixed api issue.

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
+let apiLink = 'http://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+let arrayStad = [];
 const app = Vue.createApp({
     data() {
+
         return {
-            City: '',
-            apiData: null
+            City: null,
+            apiData: null,
         }
-    },
-    mounted() {
-        // API call when the component is mounted
-        fetch('http://api.openweathermap.org/data/2.5/weather?units=metric&q=amsterdam&appid=ef63fae71b11f8ef4dd1fd8a5b7a5c87')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                // Handle the fetched data here
-                this.apiData = data;
-            })
-            .catch(error => {
-                // Handle errors
-                console.error('There was a problem with the fetch operation:', error);
-            });
     },
     methods: {
         onSubmit() {
-            console.log(this.City);
+            City = this.City;
+            let APIid = '&appid=ef63fae71b11f8ef4dd1fd8a5b7a5c87';
+            console.log(apiLink + `${City}&appid=ef63fae71b11f8ef4dd1fd8a5b7a5c87`);
+            // API call when the component is mounted
+            fetch(apiLink + `${City}&appid=ef63fae71b11f8ef4dd1fd8a5b7a5c87`)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    // Handle the fetched data here
+                    console.log(data);
+                })
+                .catch(error => {
+                    // Handle errors
+                    console.error('There was a problem with the fetch operation:', error);
+                });
+
+            console.log(City);
         }
     },
 })
