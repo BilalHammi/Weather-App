dbf929021dec3d64c33593288732f3bd77ee2b01
BilalHammi <Bilalhammi45@gmail.com>
2024-01-03 16:37:58 +0100

    Added Day function (still in process)

--- main.js
+++ main.js
@@ -7,10 +7,41 @@ const app = Vue.createApp({
             City: null,
             Country_name: null,
             Average_temp: null,
+            cloud_speed: null,
             midImg: null,
         }
     },
     methods: {
+        DateFunction() {
+            let newDate = new Date();
+            let dayToday = newDate.getDay();
+            switch (dayToday) {
+                case 1:
+                    console.log("Day 1");
+                    break;
+                case 2:
+                    console.log("Day 2");
+                    break;
+                case 3:
+                    console.log("Day 3");
+                    break;
+                case 4:
+                    console.log("Day 4");
+                    break;
+                case 5:
+                    console.log("Day 5");
+                    break;
+                case 6:
+                    console.log("Day 6");
+                    break;
+                case 7:
+                    console.log("Day 7");
+                    break;
+                default:
+                    break;
+            }
+            console.log(dayToday);
+        },
         onSubmit() {
             City = this.City;
 
@@ -33,13 +64,15 @@ const app = Vue.createApp({
                     console.log(this.midImg = data.weather[0].icon);
                     console.log(`https://openweathermap.org/img/wn/${this.midImg}@2x.png`);
                     console.log(this.Average_temp = data.main.temp + "°C");
+                    console.log(this.cloud_speed = data.wind.speed);
                 })
                 .catch(error => {
                     // Handle errors
                     console.error('There was a problem with the fetch operation:', error);
                 });
-
+            this.DateFunction();
             console.log(City);
         }
+
     },
 })
