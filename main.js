ebf691db85d71cc2be57832281501a5e5d133edf
BilalHammi <Bilalhammi45@gmail.com>
2024-01-06 21:20:44 +0100

    Added weatherHtml.

--- main.js
+++ main.js
@@ -42,44 +42,38 @@ const app = Vue.createApp({
                     break;
             }
             console.log(dayToday);
-        },
-
-        weatherFunction(weatherId) {
-            console.log(weatherId);
-        },
-        onSubmit() {
-            City = this.City;
-
-            if (City.length > 0) {
-                console.log(this.counter += 1);
-            } else {
-                console.log(this.counter = null);
-            }
-            // API call when the component is mounted
-            fetch(apiLink + `${City}&appid=ef63fae71b11f8ef4dd1fd8a5b7a5c87`)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    // Handle the fetched data here
-                    console.log(this.Country_name = data.sys.country + ",");
-                    console.log(this.midImg = data.weather[0].icon);
-                    console.log(`https://openweathermap.org/img/wn/${this.midImg}@2x.png`);
-                    console.log(this.Average_temp = data.main.temp + "°C");
-                    console.log(this.cloud_speed = data.wind.speed);
-                    this.weatherFunction(data.weather[0].id);
-                })
-                .catch(error => {
-                    // Handle errors
-                    console.error('There was a problem with the fetch operation:', error);
-                });
-            this.DateFunction();
-
-            console.log(City);
         }
-
     },
+    onSubmit() {
+        City = this.City;
+
+        if (City.length > 0) {
+            console.log(this.counter += 1);
+        } else {
+            console.log(this.counter = null);
+        }
+        // API call when the component is mounted
+        fetch(apiLink + `${City}&appid=ef63fae71b11f8ef4dd1fd8a5b7a5c87`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(this.Country_name = data.sys.country + ",");
+                console.log(this.midImg = data.weather[0].icon);
+                console.log(`https://openweathermap.org/img/wn/${this.midImg}@2x.png`);
+                console.log(this.Average_temp = data.main.temp + "°C");
+                console.log(this.cloud_speed = data.wind.speed);
+                this.weatherFunction(data.weather[0].id);
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
+        this.DateFunction();
+
+        console.log(City);
+    }
+
 })
