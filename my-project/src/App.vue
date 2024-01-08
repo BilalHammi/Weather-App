30658a7a528a42ffe288428dc96dd8cd1982265a
BilalHammi <Bilalhammi45@gmail.com>
2024-01-08 22:09:32 +0100

    Installed Vue + tailwind.

---
+++ my-project/src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="h-screen overflow-hidden">
+    <div class="">
+      <div>
+        <div class="flex justify-center ">
+          <form @submit.prevent="onSubmit" class="relative z-50 bottom-52">
+            <div class="mt-96">
+              <input type="text" id="City" v-model="City"
+                class="border-2 border-black w-56 rounded-lg py-2 text-center font-bold" required>
+              <input
+                class="border-2 border-black ml-3 px-3 py-2 hover:bg-black hover:text-white bg-gray-300 font-bold rounded-lg"
+                type="submit" value="Submit" />
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div class="absolute left-[49.5rem] z-50 flex justify-center mt-10 bottom-96">
+        <div
+          class="flex justify-center rounded-lg bg-linear h-72 w-80 bg-gradient-to-tl from-cyan-500 via-green-400 to-blue-500 items-center">
+          <div class="pt-3">
+            <h1 class="font-bold text-2xl"> {{ current_day }}</h1>
+            <img v-bind:src="'https://openweathermap.org/img/wn/' + midImg + '@2x.png'">
+            <h1 class="font-bold text-4xl">{{ Average_temp }}</h1>
+            <div class="flex flex-row font-bold text-xl">
+              <h6>{{ Country_name }}, </h6>
+              <h6> {{ City }}</h6>
+            </div>
+            <h6 class="font-bold">Wind speed: {{ cloud_speed }}</h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <weather-styling :weatherId="weatherId"></weather-styling>
+</template>
+<script>
+
+import weatherStyling from './components/weatherHtml.vue';
+
+let apiLink = 'http://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+
+export default {
+  components: {
+    weatherStyling
+  },
+  data() {
+    return {
+      counter: null,
+      City: null,
+      Country_name: null,
+      current_day: null,
+      Average_temp: null,
+      cloud_speed: null,
+      midImg: null,
+      weatherId: null
+    }
+  },
+  methods: {
+    DateFunction() {
+      // ... (your existing DateFunction code)
+    },
+    onSubmit() {
+      this.City = this.City.trim(); // Trim any leading/trailing spaces
+      if (this.City.length > 0) {
+        this.counter += 1; // Increment the counter
+        fetch(apiLink + `${this.City}&appid=ef63fae71b11f8ef4dd1fd8a5b7a5c87`)
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            return response.json();
+          })
+          .then(data => {
+            this.Country_name = data.sys.country;
+            this.midImg = data.weather[0].icon;
+            this.Average_temp = data.main.temp + "°C";
+            this.cloud_speed = data.wind.speed;
+            this.weatherId = data.weather[0].id;
+          })
+          .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+          });
+        this.DateFunction();
+      } else {
+        this.counter = null;
+        // Handle if the City field is empty
+      }
+    }
+  }
+}
+</script>
---
+++ my-project/src/components/weatherHtml.vue
@@ -0,0 +1,117 @@
+<template>
+  <!-- Thunderstorm -->
+  <div v-if="weatherId >= 801 && 805 > weatherId" class="overflow-hidden">
+    <section class="h-screen w-screen bg-blue-950 absolute top-0 z-10 overflow-hidden">
+      <div class="flex flex-row">
+        <div class="flex flex-col-reverse">
+          <img src="../assets/clouds.png" class="fixed z-40">
+          <div class="flex flex-row ml-12 mt-48">
+            <img v-for="index in 3" :key="index" src="../assets/Lightning Bolt.png" alt="">
+          </div>
+        </div>
+        <div class="flex flex-col-reverse ml-10">
+          <img src="../assets/clouds.png" class="fixed z-40">
+          <div class="flex flex-row ml-12 mt-48">
+            <img v-for="index in 3" :key="index" src="../assets/Lightning Bolt.png" alt="">
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-row absolute right-16 top-0">
+        <div class="flex flex-col-reverse">
+          <img src="../assets/clouds.png" class="fixed z-40">
+          <div class="flex flex-row ml-12 mt-48">
+            <img v-for="index in 3" :key="index" src="../assets/Lightning Bolt.png" alt="">
+          </div>
+        </div>
+        <div class="flex flex-col-reverse ml-10">
+          <img src="../assets/clouds.png" class="fixed z-40">
+          <div class="flex flex-row ml-12 mt-48">
+            <img v-for="index in 3" :key="index" src="../assets/Lightning Bolt.png" alt="">
+          </div>
+        </div>
+      </div>
+      <section class="w-screen bg-green-600 h-40 absolute bottom-0 z-10"></section>
+    </section>
+
+    <!-- Any other content related to WeatherStyling -->
+  </div>
+  <!-- Thunderstorm -->
+  <!-- drizzle  -->
+  <div v-else-if="weatherId = 300 && 322 > weatherId" class="z-50 absolute bg-black w-96 h-96">
+    <h1>Weather ID: {{ weatherId }}</h1>
+    <div>
+      <div>
+        <img src="./assets/clouds.png" class="absolute z-50 mt-28">
+        <img src="./assets/clouds.png" class="absolute z-50 mt-28 left-[30rem]">
+      </div>
+      <div>
+        <img src="./assets/clouds.png" class="absolute z-50 mt-28 right-10">
+        <img src="./assets/clouds.png" class="absolute z-50 mt-28 right-[30rem]">
+      </div>
+    </div>
+    <section class="h-screen w-screen bg-blue-400"></section>
+    <section class="w-screen bg-green-600 h-40"></section>
+    <!-- Any other content related to WeatherStyling -->
+  </div>
+  <!-- drizzle  -->
+  <!-- clear  -->
+  <div v-else-if="weatherId = 800" class="z-50 absolute bg-black w-96 h-96">
+    <h1>Weather ID: {{ weatherId }}</h1>
+    <div>
+      <div>
+        <img src="./assets/clouds.png" class="absolute z-50 mt-28">
+        <img src="./assets/clouds.png" class="absolute z-50 mt-28 left-[30rem]">
+      </div>
+      <div>
+        <img src="./assets/clouds.png" class="absolute z-50 mt-28 right-10">
+        <img src="./assets/clouds.png" class="absolute z-50 mt-28 right-[30rem]">
+      </div>
+    </div>
+    <section class="h-screen w-screen bg-blue-400"></section>
+    <section class="w-screen bg-green-600 h-40"></section>
+    <!-- Any other content related to WeatherStyling -->
+  </div>
+  <!-- clear  -->
+  <!-- clouds  -->
+  <div v-else-if="weatherId >= 801 && 805 > weatherId" class="z-50 absolute bg-black w-96 h-96">
+    <h1>Weather ID: {{ weatherId }}</h1>
+    <div>
+      <div>
+        <img src="./assets/clouds.png" class="absolute z-50 mt-28">
+        <img src="./assets/clouds.png" class="absolute z-50 mt-28 left-[30rem]">
+      </div>
+      <div>
+        <img src="./assets/clouds.png" class="absolute z-50 mt-28 right-10">
+        <img src="./assets/clouds.png" class="absolute z-50 mt-28 right-[30rem]">
+      </div>
+    </div>
+    <section class="h-screen w-screen bg-blue-400"></section>
+    <section class="w-screen bg-green-600 h-40"></section>
+    <!-- Any other content related to WeatherStyling -->
+  </div>
+  <!-- clouds  -->
+  <!-- snow  -->
+  <div v-else-if="weatherId > 600 && 623 > weatherId" class="z-50 absolute bg-purple-500 w-96 h-96">
+    <h1>Weather ID: {{ weatherId }}</h1>
+    <section class="h-screen w-screen bg-blue-400"></section>
+    <section class="w-screen bg-green-600 h-40"></section>
+    <!-- Any other content related to WeatherStyling -->
+  </div>
+  <!-- snow  -->
+  <!-- Regen  -->
+  <div v-else="weatherId > 500 && 533 > weatherId" class="z-50 absolute bg-red-500 w-96 h-96">
+    <h1>Weather ID: {{ weatherId }}</h1>
+    <section class="h-screen w-screen bg-red-400"></section>
+    <section class="w-screen bg-green-600 h-40"></section>
+    <!-- Any other content related to WeatherStyling -->
+  </div>
+  <!-- Regen  -->
+</template>
+  
+<script>
+export default {
+  props: {
+    weatherId: Number // Adjust the prop type as per your data
+  }
+};
+</script>
