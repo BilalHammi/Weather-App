d71739ab5f01c7c92453ddbe7f38552c21f4f60b
BilalHammi <Bilalhammi45@gmail.com>
2024-01-11 11:39:28 +0100

    Fixed background issue

--- my-project/src/components/weatherHtml.vue
+++ my-project/src/components/weatherHtml.vue
@@ -52,6 +52,20 @@
           </div>
         </div>
       </div>
+      <div class="flex flex-row">
+        <div class="flex flex-col-reverse mt-5 mr-24">
+          <img src="../assets/clouds.png" class="absolute z-40">
+          <div class="flex flex-row ml-12 mt-48">
+            <img v-for="index in 3" :key="index" src="../assets/water_drop.png" alt="" class="ml-7 mb-11">
+          </div>
+        </div>
+        <div class="flex flex-col-reverse mt-5">
+          <img src="../assets/clouds.png" class="absolute z-40">
+          <div class="flex flex-row ml-12 mt-48">
+            <img v-for="index in 3" :key="index" src="../assets/water_drop.png" alt="" class="ml-7 mb-11">
+          </div>
+        </div>
+      </div>
       <section class="w-screen bg-green-600 h-40 absolute bottom-0"></section>
     </section>
 
@@ -108,6 +122,11 @@
       <div class="w-full flex justify-center">
         <img :src="'https://openweathermap.org/img/wn/' + midImg + '@4x.png'" alt="" class="mt-2">
       </div>
+      <section class="w-screen bg-green-600 h-40 absolute bottom-0 z-10"></section>
+    </section>
+  </div>
+  <div v-else="weatherId == 800" class="overflow-hidden">
+    <section class="h-screen w-screen bg-blue-400 absolute top-0 z-10 overflow-hidden">
       <section class="w-screen bg-green-600 h-40 absolute bottom-0 z-10"></section>
     </section>
   </div>
